6b4e133f57450ef14bf4081c19fee16fea50ba6e
Daffa Zachari <daffazachari29@gmail.com>
2024-10-21 01:40:48 +0700

    Add files via upload

---
+++ Daffa Zachari_2311103118_Modul 2/latihanp4daffazachari/Menu.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanp4daffazachari;
+
+/**
+ *
+ * @author Daffa Zachari 
+ * 2311103118
+ * S1SI-07-C
+ */
+
+class Menu {
+    String namaItem;
+    double harga;
+    int kategori; // 1: Pembuka, 2: Utama, 3: Minuman
+
+    public Menu(String namaItem, double harga, int kategori) {
+        this.namaItem = namaItem;
+        this.harga = harga;
+        this.kategori = kategori;
+    }
+
+    // Method untuk menampilkan informasi menu
+    public void tampilkanInfo() {
+        String jenis = "";
+        switch (kategori) {
+            case 1:
+                jenis = "Pembuka";
+                break;
+            case 2:
+                jenis = "Utama";
+                break;
+            case 3:
+                jenis = "Minuman";
+                break;
+        }
+        System.out.println(namaItem + " - Kategori: " + jenis + " - Harga: Rp " + harga);
+    }
+}
+
+
