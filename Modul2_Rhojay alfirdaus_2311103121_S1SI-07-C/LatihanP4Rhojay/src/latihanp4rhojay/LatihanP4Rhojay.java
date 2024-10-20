c8027aaac25a1c1ce685945b851ab09440bb43d6
rhojayalfirdaus <145036557+rhojayalfirdaus@users.noreply.github.com>
2024-10-21 00:43:18 +0700

    Add files via upload

---
+++ Modul2_Rhojay alfirdaus_2311103121_S1SI-07-C/LatihanP4Rhojay/src/latihanp4rhojay/LatihanP4Rhojay.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihanp4rhojay;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rhojay Alfirdaus 2311103121 S1SI07C
+ */
+public class LatihanP4Rhojay {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Pesanan pesanan = new Pesanan(10);
+        char tambahPesanan;
+
+        do {
+            System.out.print("Masukkan nama item : ");
+            String namaItem = scanner.nextLine();
+            System.out.print("Masukkan harga item : ");
+            double hargaItem = scanner.nextDouble();
+            System.out.print("Masukkan kategori (1: Pembuka, 2: Utama, 3: Minuman) : ");
+            int kategoriItem = scanner.nextInt();
+            scanner.nextLine();
+            Menu item = new Menu(namaItem, hargaItem, kategoriItem);
+            pesanan.tambahPesanan(item);
+            System.out.print("Apakah ingin menambah pesanan lagi (Y/N) : ");
+            tambahPesanan = scanner.next().charAt(0);
+            scanner.nextLine();
+        } while (tambahPesanan == 'y' || tambahPesanan == 'Y');
+
+        pesanan.tampilkanPesanan();
+        double diskon = pesanan.hitungDiskon();
+        if (diskon > 0) {
+            System.out.println("Anda mendapatkan diskon 10% sebesar Rp." + diskon);
+        }
+        System.out.println("Total yang harus dibayar: Rp." + (pesanan.totalHarga - diskon));
+    }
+}
---
+++ Modul2_Rhojay alfirdaus_2311103121_S1SI-07-C/LatihanP4Rhojay/src/latihanp4rhojay/Menu.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanp4rhojay;
+
+/**
+ *
+ * @author Rhojay Alfirdaus
+ * 2311103121
+ * S1SI07C
+ */
+public class Menu {
+    String namaItem;
+ double harga;
+ int kategori;
+ 
+ public Menu(String namaItem, double harga, int kategori){
+ this.namaItem = namaItem;
+ this.harga = harga;
+ this.kategori = kategori;
+ }
+public void tampilkanInfo(){
+ String jenis = "";
+ switch (kategori){
+ case 1: jenis = "Pembuka";
+ break;
+ 
+ case 2: jenis = "Utama";
+ break;
+ 
+ case 3: jenis = "Minuman";
+ break;
+ }
+ System.out.println(namaItem + " - Kategori: " + jenis + " - Harga: Rp." + harga);
+ }
+}
+
