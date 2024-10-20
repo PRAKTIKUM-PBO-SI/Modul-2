25efee1b1a542be32329d7ee9dc95fe7a569cd86
chrisshtss17 <chrishotasi697@gmail.com>
2024-10-20 23:44:59 +0700

    Add files via upload

---
+++ ChrisHotasiRajagukguk_2311103126/LatihanP4ChrisHotasiRajagukguk/src/latihanp4chrishotasirajagukguk/Menu.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanp4chrishotasirajagukguk;
+
+/**
+ *
+ * @author Chris Hotasi Rajagukguk
+ */
+public class Menu {
+    String namaItem;
+    double harga;
+    int kategori; // 1: Pembuka, 2: Utama, 3: Minuman
+    
+    public Menu(String namaItem, double harga, int kategori){
+        this.namaItem = namaItem;
+        this.harga = harga;
+        this.kategori = kategori;
+    }
+        // Method untuk menampilkan informasi menu
+    public void tampilkanInfo() {
+        String jenis = "";
+        switch (kategori) {
+            case 1 -> jenis = "Pembuka";
+            case 2 -> jenis = "Utama";
+            case 3 -> jenis = "Minuman";
+        }
+        System.out.println(namaItem + " - Kategori: " + jenis + " - Harga: Rp " + harga);
+    }
+}
